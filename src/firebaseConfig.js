99f256d3e6255d5e4ec333b15f5f95360a24625c
HotdogWithMayo <92586956+Thestudyguy@users.noreply.github.com>
2023-05-21 02:51:15 +0800

    Update firebaseConfig.js
    creds removed

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,1 @@
-import { initializeApp } from "firebase/app";
-import {getDatabase} from "firebase/database";
-const firebaseConfig = {
-  apiKey: "AIzaSyDuIKxhC8WCBvFyM7I3GOj-0LeOd5jlYxg",
-  authDomain: "lagrosa-dc2bc.firebaseapp.com",
-  databaseURL: "https://lagrosa-dc2bc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lagrosa-dc2bc",
-  storageBucket: "lagrosa-dc2bc.appspot.com",
-  messagingSenderId: "307681799709",
-  appId: "1:307681799709:web:fcb78a1390a465f3c7a0f4",
-  measurementId: "G-KDY8ND3PZW"
-};
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+
